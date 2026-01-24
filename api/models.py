b97d42ae91d247990ffeba4415714a96e08383cc
Ebenezer Lamptey <87435572+nlankwei5@users.noreply.github.com>
2026-01-24 07:49:21 +0000

    Add Stock model with fields for stock name, price, and timestamp; add database index.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Stock(models.Model):
+    stock = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=4)
+    timestamp = models.DateTimeField(db_index=True)
+
+
+    class Meta:
+        indexes =[
+            models.Index(fields=['timestamp', 'stock']),
+        ]
+
