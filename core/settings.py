f8ec86ac2e78c29eee287d46312d9e8cd00c166d
Ebenezer Lamptey <87435572+nlankwei5@users.noreply.github.com>
2026-02-02 14:39:28 +0000

    Add redis for caching prices

--- core/settings.py
+++ core/settings.py
@@ -135,3 +135,14 @@ CELERY_RESULT_EXTENDED = True
 
 
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
